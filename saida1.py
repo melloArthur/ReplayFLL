fc4e884ac5390e6b07e5002774db09dbbae10d71
PC_Léo <leocesar.rj@gmail.com>
2021-03-30 22:35:47 -0300

    Novo padrão de arquivos

---
+++ saida1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env pybricks-micropython
+
+from main import *
+
+# andar 10 cm
+Acelera(10, 50)
+# virar 40°
--- teste_aceleracao1.py
+++ teste_aceleracao1.py
@@ -25,26 +25,24 @@ gabriel = DriveBase(esquerda, direita, wheel_diameter=88,axle_track=90) #Ajustar
 
 #Funções
 def Acelera(qp_max, qf)
-    gabriel.straight(qf)
+    # dados iniciais ----------------------
+    qi     =    0               # posição inicial (em mm)
     
-    # # dados iniciais ----------------------
-    # qi     =    0               # posição inicial (em mm)
+    # primeiro cálculo
+    Dq =  qf - qi           # variação da posição (distância, em mm)
     
-    # # primeiro cálculo
-    # Dq =  qf - qi           # variação da posição (distância, em mm)
+    # determinação das outras variáveis 
+    tf = 15*Dq/(8*qp_max)       # tempo do movimento
+    a3 = 16*qp_max/(3*tf**2)
+    a4 = -8*qp_max/(tf**3)
+    a5 = 16*qp_max/(5*tf**4)
     
-    # # determinação das outras variáveis 
-    # tf = 15*Dq/(8*qp_max)       # tempo do movimento
-    # a3 = 16*qp_max/(3*tf**2)
-    # a4 = -8*qp_max/(tf**3)
-    # a5 = 16*qp_max/(5*tf**4)
+    # inicio do tempo
+    t = 0
+    dt = 0.01
+    vec_array = []
     
-    # # inicio do tempo
-    # t = 0
-    # dt = 0.01
-    # vec_array = []
-    
-    # # loop do cálculo dos valores de velocidade
+    # loop do cálculo dos valores de velocidade
     # while t < tf:
     #     qp = round(3*a3*t**2 + 4*a4*t**3 + (5*a5*t**4), 2)
     #     t += dt
@@ -54,6 +52,29 @@ def Acelera(qp_max, qf)
     # for i in range(0, len(vec_array)):
     #     gabriel.drive(vec_array[i],0)
     #     wait(dt*1000)
+
+    # tentativa de fazer o loop pela posição e não pela velocidade #########################################
+    q_ant = 0
+    space_array = []
+    while t < tf:
+        # qp = round(3*a3*t**2 + 4*a4*t**3 + (5*a5*t**4), 2)
+        
+        q = a3*t**3 + a4*t**4 + a5*t**5
+        dist = q - q_ant
+        
+        vec_array.append(dist)
+        t += dt
+        q_ant = q
+
+    for i in range(0, len(space_array)):
+        gabriel.straight(space_array[i])
+        wait(dt*1000)
+    # tentativa de fazer o loop pela posição e não pela velocidade #########################################
+
+    # tentativa p/ o giro sem gyro ##################################################
+    # gabriel.turn(ang, vel)
+
+    # tentativa p/ o giro sem gyro ##################################################
 
 
 def SegLinhaPID(potencia, vezes)
