15ca2bc3fb21acced8b3d733eed009226fbe1efc
Arthur Oliveira <zompy@riseup.net>
2021-04-09 22:40:16 -0300

    Leo: ajustes funções e correção wheel_diameter

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env pybricks-micropython
-
-#Imports
-
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
@@ -10,65 +7,56 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
-#Declaração das portas
-
 esquerda = Motor(Port.B)
 direita = Motor(Port.C)
-EsqCor = ColorSensor(Port.S3)
+EsqCor = ColorSensor(Port.S1)
 DirCor = ColorSensor(Port.S4)
 
-gabriel = DriveBase(esquerda, direita, wheel_diameter=88,axle_track=90) #Ajustar valores
-
-#Código
+gabriel = DriveBase(esquerda, direita, wheel_diameter=100,axle_track=90) #Ajustar valores
 
 
-#Funções
-def Acelera(qp_max, qf)
-    gabriel.straight(qf)
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+
+# Write your program here.
+ev3.speaker.beep()
+
+def Acelera(qp_max, qf):
     
-    # # dados iniciais ----------------------
-    # qi     =    0               # posição inicial (em mm)
     
-    # # primeiro cálculo
-    # Dq =  qf - qi           # variação da posição (distância, em mm)
+    # dados iniciais ----------------------
+    qi     =    0               # posição inicial (em mm)
     
-    # # determinação das outras variáveis 
-    # tf = 15*Dq/(8*qp_max)       # tempo do movimento
-    # a3 = 16*qp_max/(3*tf**2)
-    # a4 = -8*qp_max/(tf**3)
-    # a5 = 16*qp_max/(5*tf**4)
+    # primeiro cálculo
+    Dq =  qf - qi           # variação da posição (distância, em mm)
     
-    # # inicio do tempo
-    # t = 0
-    # dt = 0.01
-    # vec_array = []
+    # determinação das outras variáveis 
+    tf = 15*Dq/(8*qp_max)       # tempo do movimento
+    a3 = 16*qp_max/(3*tf**2)
+    a4 = -8*qp_max/(tf**3)
+    a5 = 16*qp_max/(5*tf**4)
     
-    # # loop do cálculo dos valores de velocidade
-    # while t < tf:
-    #     qp = round(3*a3*t**2 + 4*a4*t**3 + (5*a5*t**4), 2)
-    #     t += dt
-    #     vec_array.append(qp)
+    # inicio do tempo
+    t = 0
+    dt = 0.01
+    vec_array = []
+    
+    # loop do cálculo dos valores de velocidade
+    while t < tf:
+        qp = round(3*a3*t**2 + 4*a4*t**3 + (5*a5*t**4), 2)
+        t += dt
+        vec_array.append(qp)
 
-    # # loop do movimento
-    # for i in range(0, len(vec_array)):
-    #     gabriel.drive(vec_array[i],0)
-    #     wait(dt*1000)
+    # loop do movimento
+    for i in range(0, len(vec_array)):
+        gabriel.drive(vec_array[i],0)
+        wait(dt*1000)
 
+ev3.speaker.beep()
 
-def SegLinhaPID(potencia, vezes)
-    #Definir valores de calibração
-    kp =
-    ki =
-    kd = 
-
-    UltErro = 0
-    erro = 0
-    contador = 0
-    
-    while contador < vezes: #Loop para o movimento.
-        erro = EsqCor.reflection() - DirCor.reflection()
-        integral = integral + erro
-        derivativa = erro - UltErro
-        curva = erro * kp + integral * ki + derivativa * kp
-        gabriel.drive(potencia,curva)
+Acelera(600, 600)
--- saida1.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env pybricks-micropython
-
-from main import *
-
-# andar 10 cm
-Acelera(10, 50)
-# virar 40°
