ee0cf20e700af8d43cd2c97c3c8467310c8ca0f8
Arthur Oliveira <zompy@riseup.net>
2021-04-12 22:40:17 -0300

    Sem grandes alterações.

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def latencia():
 # for i in range(4):
 #     gabriel.turn(45) #Dansa gatinho dansa
 #     latencia()
-#     gabriel.turn(-45) #só um teste po
+#     gabriel.turn(-45) #só um teste p
 #     latencia()
 
 
